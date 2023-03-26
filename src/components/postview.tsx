c6eec96b8d10f9392c5d4b23ed0118e991b7e677
Dindault Lucas <dindault.lucas@gmail.com>
2023-03-26 17:48:35 +0200

    fix vercel recommandations

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -13,11 +13,11 @@ export const PostView = (props: PostWithUser) => {
 
   return (
     <div key={post.id} className="flex gap-3 p-4 border-b border-slate-400">
-      <Image src={author.profilePicture} className="h-14 w-14 rounded-full" alt={`@${author.username}`} width={56} height={56} />
+      <Image src={author.profilePicture} className="h-14 w-14 rounded-full" alt={`@${author.username ?? ""}`} width={56} height={56} />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <Link href={`/@${author.username}`}>
-            <span>{`@${author.username}`}</span>
+          <Link href={`/@${author.username ?? ""}`}>
+            <span>{`@${author.username ?? ""}`}</span>
           </Link>
           <Link href={`/post/${post.id}`}>
             <span className="font-thin">{` · ${dayjs(post.createdAt).fromNow()}`}</span>
