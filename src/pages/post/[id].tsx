7180f343acb43ff46ff704a47911c4b1845e2607
Dindault Lucas <dindault.lucas@gmail.com>
2023-03-27 00:08:56 +0200

    separate ssg helper, full post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,76 +1,42 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
-import { createProxySSGHelpers } from "@trpc/react-query/ssg"
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db";
-import superjson from "superjson"
 import { PageLayout } from "~/components/layout";
-import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 import { PostView } from "~/components/postview";
 
-const ProfileFeed = (props: {userId: string}) => {
-  const {data, isLoading} = api.posts.getPostsByUserId.useQuery({
-    userId: props.userId
-  })
+const SinglePostPage: NextPage<{id: string}> = ({id}) => {
 
-  if (isLoading) return <LoadingPage />
-
-  if (!data || data.length === 0) return <div>User has not posted</div>
-
-  return <div className="flex flex-col">
-    {data.map((fullPost) => (
-      <PostView {...fullPost} key={fullPost.post.id}/>
-    ))}
-  </div>
-}
-
-const ProfilePage: NextPage<{username: string}> = ({username}) => {
-
-  const {data} = api.profile.getUserByUsername.useQuery({
-    username,
-  })
+  const {data} = api.posts.getPostById.useQuery({
+    id
+  });
 
   if (!data) return <div>404</div>  
 
   return (
     <>
       <Head>
-        <title>{data.username}</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
       <PageLayout>
-        <div className="relative h-36 bg-slate-600">
-          <Image className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black" src={data.profilePicture} alt={`${data.username ?? ""}'s profile pic`} width={128} height={128} />
-        </div>
-        <div className="h-[64px]"></div>
-        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
-        <div className="w-full border-b border-slate-400"></div>
-        <ProfileFeed userId={data.id} />
+        <PostView {...data} />
       </PageLayout>
     </>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createProxySSGHelpers({
-    router: appRouter,
-    ctx: { prisma, userId: null },
-    transformer: superjson,
-  });
+  const ssg = generateSSGHelper();
+  const id = context.params?.id;
 
-  const slug = context.params?.slug;
+  if (typeof id !== "string") throw new Error("no id");
 
-  if (typeof slug !== "string") throw new Error("no slug");
-
-  const username = slug.replace("@", "");
-
-  await ssg.profile.getUserByUsername.prefetch({ username })
+  await ssg.posts.getPostById.prefetch({id})
 
   return {
     props: {
       trpcState: ssg.dehydrate(),
-      username,
+      id,
     },
   }
 }
@@ -79,4 +45,4 @@ export const getStaticPaths = () => {
   return {paths: [], fallback: "blocking"}
 }
 
-export default ProfilePage;
+export default SinglePostPage;
