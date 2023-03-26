4e8b2a3672b8d2c3f1af733027e21e60be4f8760
Dindault Lucas <dindault.lucas@gmail.com>
2023-03-26 17:31:40 +0200

    Render posts on profile page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,5 @@
 import { clerkClient } from "@clerk/nextjs/server";
+import { Post } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
@@ -6,6 +7,30 @@ import { z } from "zod";
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
+
+const addUserDataToPosts = async (posts: Post[]) => {
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+  ).map(filterUserForClient);
+
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+
+    if (!author || !author.username)
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author for post not found",
+      });
+
+      return {
+        post,
+        author,
+      }
+  })
+}
 
 // Create a ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -18,33 +43,23 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
-      orderBy: [
-        {createdAt: "desc"}
-      ]
+      orderBy: [{createdAt: "desc"}]
     });
 
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
-        limit: 100,
-      })
-    ).map(filterUserForClient);
-
-    return posts.map((post) => {
-      const author = users.find((user) => user.id === post.authorId);
-
-      if (!author || !author.username)
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author for post not found",
-        });
-
-        return {
-          post,
-          author,
-        }
-    })
+    return addUserDataToPosts(posts);
   }),
+
+  getPostsByUserId: publicProcedure.input(z.object({
+    userId: z.string(),
+    })).query(({ctx, input}) => ctx.prisma.post.findMany({
+      where: {
+        authorId: input.userId,
+      },
+      take: 100,
+      orderBy: [{ createdAt: "desc" }],
+    })
+    .then(addUserDataToPosts)
+  ),
 
   create: privateProcedure.input(z.object({
     content: z.string().min(1).max(280),
